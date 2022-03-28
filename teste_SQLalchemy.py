98aa95e88089977571864430d6edacc58540243d
Eduardo Caitano <83654560+Duduzyn-py@users.noreply.github.com>
2022-03-28 11:16:15 -0300

    Update teste_SQLalchemy.py

--- teste_SQLalchemy.py
+++ teste_SQLalchemy.py
@@ -6,7 +6,7 @@ class Casa(db.model):
   # cria autoaticamente a 'casa' na classe referida
 
 	def __str__(self):
-		return f'{self.id}, {self.formato}'
+		return f'Casa: {self.id}, {self.formato}'
   
 class Quarto(db.model):
 	id = db.Columm(db.integer, primary_key = True)
@@ -14,6 +14,20 @@ class Quarto(db.model):
 	dimensoes = db.Columm(db.string(224))
 	casa_id = db.Columm(db.integer, db.foreignKey(Casa.id), nullable = False)
   #casa = db.relationship("Casa")
+	def __str__(self):
+		return f'Quarto: {self.id}, {self.nome}, {self.dimensoes}, {str(self.casa)}'
+
+class Moilia(db.Model):
+	id = db.Columm(db.integer, primary_key = True)
+	nome = db.Columm(db.string(254))
+	funcao = db.Columm(db.string(254))
+	material = db.Columm(db.string(254))
+	
+	def __str__(self):
+		return f'Mobilia: {self.id}, {self.nome}, {self.funcao}, {self.material}'
+		if self.quarto.Casa:
+			s += f, ' localizada em: {str(self.quarto)}'
+			
 
 c1 = Casa(formato = 'Romana')
 db.session.add(c1)
